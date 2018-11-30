89b5be7442d78a114307d4fcee8cbe6abae13a35
alexandre <alexandre.vida@valecard.com.br>
2018-11-29 23:40:54 -0200

    Atualizando Repositorio

---
+++ backup-amazon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CAMINHO_BACKUP=/home/alexandre/backup_mutillidae_amazon
+
+cd $CAMINHO_BACKUP
+
+data=$(date +%F)
+
+if [ ! -d $data ]
+then
+	mkdir $data
+
+fi 
+
+tabelas=$(mysql -u root mutillidae -e "show tables;" | grep -v Tables)
+
+#echo $tabelas
+
+for tabela in $tabelas
+do
+    mysqldump -u root mutillidae $tabela > $CAMINHO_BACKUP/$data/$tabela.sql
+done
+
+aws s3 sync $CAMINHO_BACKUP s3://curso-shell-script2
